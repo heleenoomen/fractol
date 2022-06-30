8c666c9419b13e8edc5748817699368b5f7643b4
Heleen Oomen <hoomen@1-C-15.42heilbronn.de>
2022-06-30 11:49:32 +0200

    keys_h header

--- include/keys.h
+++ include/keys.h
@@ -6,24 +6,86 @@
 /*   By: hoomen <hoomen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 19:33:54 by hoomen            #+#    #+#             */
-/*   Updated: 2022/06/29 19:40:05 by hoomen           ###   ########.fr       */
+/*   Updated: 2022/06/30 11:48:07 by hoomen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef KEYS_H
 # define KEYS_H
 
+/* ************************************************ */
+/*                                                  */
+/* 		keyboard & mouse button codes (Mac OS)		*/
+/*									                */
+/* ************************************************ */
+
 /*
-// keyboard codes (Mac OS)
+// special keys
 */
 # define KC_ESC 53
-# define KC_Q 12
-# define KC_X 7
-# define KC_Z 6
-# define KC_R 15
-# define KC_P 35
+# define KC_SPACEBAR 49
+
+/*
+// arrow keys
+*/
+# define KC_ARROW_UP 126
+# define KC_ARROW_DOWN 125
+# define KC_ARROW_LEFT 123
+# define KC_ARROW_UP 124
+
+/*
+// numeric keys (KC = main keyboard, KC_NP = numpad)
+*/
+# define KC_1 18
+# define KC_NP_1 83
+# define KC_2 19
+# define KC_NP_2 84
+# define KC_3 20
+# define KC_NP_3 85
+# define KC_4 21
+# define KC_NP_4 86
+# define KC_5 23
+# define KC_NP_5 87
+# define KC_6 22
+# define KC_NP_6 88
+# define KC_7 26
+# define KC_NP_7 89
+# define KC_8 28
+# define KC_NP_8 91
+# define KC_9 25
+# define KC_NP_9 92
+# define KC_0 29
+# define KC_NP_0 82
+
+/*
+// alphabetic keys
+*/
+# define KC_A 0
 # define KC_B 11
+# define KC_C 8
+# define KC_D 2
+# define KC_E 14
+# define KC_F 3
+# define KC_G 5
+# define KC_H 4
 # define KC_I 34
+# define KC_J 38
+# define KC_K 40
+# define KC_L 37
+# define KC_M 46
+# define KC_N 45
+# define KC_O 31
+# define KC_P 35
+# define KC_Q 12
+# define KC_R 15
+# define KC_S 1
+# define KC_T 17
+# define KC_U 32
+# define KC_V 9
+# define KC_W 13
+# define KC_X 7
+# define KC_Y 16
+# define KC_Z 6
 
 /*
 // mouse button codes
@@ -31,7 +93,5 @@
 # define SCROLL_UP 5
 # define SCROLL_DOWN 4
 # define RIGHT_CLICK 2
-
-
 
 #endif
